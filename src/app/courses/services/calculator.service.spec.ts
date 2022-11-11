76cccea1b9c509693d49762747264412cc9964e5
raunakg <raunakguptarg@gmail.com>
2022-11-11 23:06:02 +0530

    first test

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,24 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
+describe("CalculatorService", () => {
+  it("should add two numbers", () => {
+    const logger = new LoggerService();
+
+    spyOn(logger, "log");
+    const calculator = new CalculatorService(logger);
+
+    const result = calculator.add(2, 2);
+
+    expect(result).toBe(4);
+    expect(logger.log).toHaveBeenCalledTimes(1);
+  });
+
+  it("should subtract two numbers", () => {
+    const calculator = new CalculatorService(new LoggerService());
+
+    const result = calculator.subtract(2, 2);
+
+    expect(result).toBe(0, "unexpected subtraction result");
+  });
+});
