b42d63e68c53a4c700d0f2073769146c07e04eeb
raunakg <raunakguptarg@gmail.com>
2022-11-11 23:24:48 +0530

    testbed beginner

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,3 +1,4 @@
+import { TestBed } from "@angular/core/testing";
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
@@ -7,7 +8,14 @@ describe("CalculatorService", () => {
   beforeEach(() => {
     loggerSpy = jasmine.createSpyObj("LoggerService", ["log"]);
 
-    calculator = new CalculatorService(loggerSpy);
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy },
+      ],
+    });
+
+    calculator = TestBed.inject(CalculatorService);
   });
 
   it("should add two numbers", () => {
