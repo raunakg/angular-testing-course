197b58b59dd8eea3b7f10af5fd0452a8a1a82be7
raunakg <raunakguptarg@gmail.com>
2022-11-25 11:18:18 +0530

    data modification service

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -5,6 +5,7 @@ import {
   HttpTestingController,
 } from "@angular/common/http/testing";
 import { COURSES } from "../../../../server/db-data";
+import { Course } from "../model/course";
 
 describe("CoursesService", () => {
   let coursesService: CoursesService,
@@ -46,6 +47,26 @@ describe("CoursesService", () => {
     expect(req.request.method).toEqual("GET");
 
     req.flush(COURSES[12]);
+  });
+
+  it("should save the course data", () => {
+    const changes: Partial<Course> = {
+      titles: { description: "Testing Course" },
+    };
+    coursesService.saveCourse(12, changes).subscribe((course) => {
+      expect(course.id).toBe(12);
+    });
+
+    const req = httpTestingController.expectOne("/api/courses/12");
+
+    expect(req.request.method).toEqual("PUT");
+
+    expect(req.request.body.titles.description).toEqual("Testing Course");
+
+    req.flush({
+      ...COURSES[12],
+      ...changes,
+    });
   });
 
   afterEach(() => {
